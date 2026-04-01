cda2c349336a117cf47f3a045cc50e67e375654d
Chetan Verma <chetancodesforyou@gmail.com>
2026-04-01 14:21:54 +0530

    Adding comments
    Added comments to the main function for clarity.

---
+++ comments.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!\n"; // these double forward slashes are used add comments in any program  
+    std::cout << "It is very nice to meet you!\n"; //so i am adding my first comment though i have done this all before
+    std::cout << "Yeah!\n"; //but i am doing this all again just so i can maike a cpp repo on github
+
+    return 0;
+}
