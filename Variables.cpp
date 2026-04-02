9008502cf9a8625f052cb5cd0b3c5c49552557a3
Chetan Verma <chetancodesforyou@gmail.com>
2026-04-02 20:53:06 +0530

    Variables.cpp
    Added comments to clarify the purpose of the output statements.

--- Variables.cpp
+++ Variables.cpp
@@ -5,7 +5,7 @@ int main()
     std::cout << 5;       // print the literal number `5`
     std::cout << -6.7;    // print the literal number `-6.7`
     std::cout << 'H';     // print the literal character `H`
-    std::cout << "Hellow"; // print the literal text `Hellow`
+    std::cout << "Hellow"; // print the literal text `Hellow` (adding comments)
 
     return 0;
 }
