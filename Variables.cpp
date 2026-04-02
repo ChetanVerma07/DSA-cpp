c5c1dbe7d503018a7e786bf46cf75fb4a3cf5368
Chetan Verma <chetancodesforyou@gmail.com>
2026-04-02 20:52:04 +0530

    Variables.cpp

--- Variables.cpp
+++ Variables.cpp
@@ -5,7 +5,7 @@ int main()
     std::cout << 5;       // print the literal number `5`
     std::cout << -6.7;    // print the literal number `-6.7`
     std::cout << 'H';     // print the literal character `H`
-    std::cout << "Hello"; // print the literal text `Hello`
+    std::cout << "Hellow"; // print the literal text `Hellow`
 
     return 0;
 }
