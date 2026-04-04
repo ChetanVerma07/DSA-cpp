ac7e7b237e018fb04019edd831839fd72e906a3d
Chetan Verma <chetancodesforyou@gmail.com>
2026-04-04 12:45:53 +0530

    Add basic I/O example in Iostream2.cpp

---
+++ Iostream2.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // for std::cout and std::endl
+
+int main()
+{
+    std::cout << "Hi!" << std::endl; // std::endl will move curser to next linee
+    std::cout << "My name is chetan." << std::endl;
+
+    return 0;
+}
